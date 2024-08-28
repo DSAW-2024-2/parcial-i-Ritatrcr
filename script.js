379aacbe739f6112bd79de144e42b922a08908b7
Rita Trindade da Cruz <ritatrcr@unisabana.edu.co>
2024-08-28 08:26:14 -0500

    add javascript and styles

---
+++ script.js
@@ -0,0 +1,12 @@
+const shareButton = document.getElementById('shareButton');
+const shareOptions = document.getElementById('shareOptions');
+    
+shareButton.addEventListener('click', function() {
+    if (shareOptions.style.display === 'none' || shareOptions.style.display === '') {
+    shareOptions.style.display = 'flex';
+    shareButton.classList.add('active-button');
+    } else {
+    shareOptions.style.display = 'none';
+    shareButton.classList.remove('active-button');
+    }
+});
